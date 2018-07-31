b114edb3429762dcc64ba2966fd4f24a157bfbcd
Phat Ma <phat.ma@hotmail.co.uk>
2018-07-31 12:51:29 +0100

    apply stores to index

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,3 +1,7 @@
 import { combineReducers } from 'redux';
 
-export default combineReducers({});
+import currentWeather from './currentWeather';
+
+export default combineReducers({
+  currentWeather
+});
