deebf8829a8742860a0c12a2d6c9b175063317db
Phat Ma <phat.ma@hotmail.co.uk>
2018-07-31 01:19:04 +0100

    basic layout using bootstrap css

--- src/components/searchBar.js
+++ src/components/searchBar.js
@@ -1,8 +1,31 @@
 import React, { Component } from 'react';
 
 class SearchBar extends Component {
+  state = {
+    searchBarValue: '',
+    searchTerm: ''
+  };
+
   render() {
-    return <div>SearchBar</div>;
+    return (
+      <form>
+        <div className="form-group">
+          <input
+            type="text"
+            className="form-control"
+            id="search-input"
+            placeholder="Enter a city..."
+            aria-describedby="cityHelp"
+          />
+          <small id="cityHelp" className="form-text text-muted">
+            for example, London
+          </small>
+        </div>
+        <button type="button" className="btn btn-info">
+          Search
+        </button>
+      </form>
+    );
   }
 }
 
