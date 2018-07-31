7f2547785dcf9a696224178aa7db537816c06be3
Phat Ma <phat.ma@hotmail.co.uk>
2018-07-31 01:01:48 +0100

    create searchbar placeholder

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,14 @@
 import React, { Component } from 'react';
 
+import SearchBar from './searchBar';
+
 class App extends Component {
   render() {
-    return <div>Hello World</div>;
+    return (
+      <div>
+        <SearchBar />
+      </div>
+    );
   }
 }
 
