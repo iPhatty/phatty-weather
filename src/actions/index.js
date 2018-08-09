b27d9c22abde2fa84c63007898f52c0a18f3ff4c
Phat Ma <phat.ma@hotmail.co.uk>
2018-08-09 15:36:52 +0100

    create initial fetch current weather action

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,18 @@
 import { FETCH_CURRENT_WEATHER } from './types';
+import axios from 'axios';
+
+const env = require('env2')('../../.env');
+
+export function fetchCurrent(city) {
+  const url = `api.openweathermap.org/data/2.5/weather?q=${city}&appid=${
+    process.env.API_KEY
+  }`;
+  console.log(url);
+  axios.get(url).then(data => {
+    console.log(data);
+    return {
+      type: FETCH_CURRENT_WEATHER,
+      payload: data
+    };
+  });
+}
