42d889bffacc85adce08b60b95025aa95178f682
Phat Ma <phat.ma@hotmail.co.uk>
2018-08-13 00:09:51 +0100

    display results

--- src/components/searchResults.js
+++ src/components/searchResults.js
@@ -2,7 +2,18 @@ import React from 'react';
 import { connect } from 'react-redux';
 
 const SearchResult = props => {
-  return <div>Search Result</div>;
+  const { name, main, weather } = props.currentWeather;
+  if (Object.keys(props.currentWeather).length > 0) {
+    return (
+      <div>
+        <h2>{name}</h2>
+        <h3>{weather[0].main}</h3>
+        <h3>Temp: {main.temp}&deg;C</h3>
+        <h3>Humidity: {main.humidity}%</h3>
+      </div>
+    );
+  }
+  return <div>Phatty Weather goes here...</div>;
 };
 
 function mapStateToProps(state) {
