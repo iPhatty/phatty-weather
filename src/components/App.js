d54f6a5aa2fcff699e799dcb496c11b1f445f5c8
Phat Ma <phat.ma@hotmail.co.uk>
2018-08-14 16:41:10 +0100

    flex app container

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 
+import '../styling/App.css';
+
 import SearchBar from './searchBar';
 import SearchResult from './searchResult';
 
 class App extends Component {
   render() {
     return (
-      <div>
+      <div className="app-container">
         <SearchBar />
         <SearchResult />
       </div>
