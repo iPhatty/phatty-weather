68b46ec4a4f4f15b5828eb2e4dc6b6a5f292e3dc
Phat Ma <phat.ma@hotmail.co.uk>
2018-08-14 17:33:42 +0100

    center components

--- src/components/searchResult.js
+++ src/components/searchResult.js
@@ -8,12 +8,15 @@ const SearchResult = props => {
       <div>
         <h2>{name}</h2>
         <h3>{weather[0].main}</h3>
-        <h3>Temp: {main.temp}&deg;C</h3>
+        <h3>
+          Temp: {Math.round(main.temp)}
+          &deg;C
+        </h3>
         <h3>Humidity: {main.humidity}%</h3>
       </div>
     );
   }
-  return <div>Phatty Weather goes here...</div>;
+  return <h1>Phatty Weather</h1>;
 };
 
 function mapStateToProps(state) {
